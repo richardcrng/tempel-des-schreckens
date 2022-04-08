992efdd74cf43c9a61817cb23bbe3e7af785fab6
Richard Ng <richardcrng@gmail.com>
2022-04-08 20:26:44 +0100

    Format files

--- server/src/player/controllers.ts
+++ server/src/player/controllers.ts
@@ -2,9 +2,7 @@ import {
   ClientEvent,
   ClientEventListeners,
 } from "../../../client/src/types/event.types";
-import {
-  Player,
-} from "../../../client/src/types/game.types";
+import { Player } from "../../../client/src/types/game.types";
 import { GameManager } from "../game/model";
 import { getColors } from "../utils";
 
@@ -19,7 +17,6 @@ export const joinPlayerToGame: ClientEventListeners[ClientEvent.JOIN_GAME] = (
     colors: getColors(5),
   });
 };
-
 
 export const updatePlayer: ClientEventListeners[ClientEvent.UPDATE_PLAYER] = (
   gameId,
