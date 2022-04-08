3ee13147135edae32c190641a2ca02adee6767e0
Richard Ng <richardcrng@gmail.com>
2022-04-08 18:45:53 +0100

    Export SERVER_IO constant

--- server/src/server.ts
+++ server/src/server.ts
@@ -6,16 +6,16 @@ import { addGameListeners } from "./game/listeners";
 import { addPlayerListeners } from "./player/listeners";
 
 const httpServer = createServer(app);
-const io: ServerIO = new Server(httpServer, {
+export const SERVER_IO: ServerIO = new Server(httpServer, {
   cors: {
     origin: "*",
     methods: ["GET", "POST"],
   },
 });
 
-io.on("connection", (socket: ServerSocket) => {
-  addGameListeners(socket, io);
-  addPlayerListeners(socket, io);
+SERVER_IO.on("connection", (socket: ServerSocket) => {
+  addGameListeners(socket, SERVER_IO);
+  addPlayerListeners(socket, SERVER_IO);
 });
 
 export default httpServer;
