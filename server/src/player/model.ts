078ae0e4881a7601fcd061f83f1be1f620b679c3
Richard Ng <richardcrng@gmail.com>
2022-04-08 18:51:57 +0100

    Create basic models for game and players

---
+++ server/src/player/model.ts
@@ -0,0 +1,76 @@
+import { cloneDeep } from "lodash";
+import { ServerEvent } from "../../../client/src/types/event.types";
+import { Player } from "../../../client/src/types/game.types";
+import { GameManager, Operation } from "../game/model";
+
+export class PlayerManager {
+  constructor(
+    public gameManager: GameManager,
+    public socketId: string,
+    public aliasSocketIds: string[] = []
+  ) {}
+
+  _broadcast(): void {
+    this._withPointer((pointer) => {
+      this.gameManager.io.emit(
+        ServerEvent.PLAYER_UPDATED,
+        this.socketId,
+        pointer
+      );
+    });
+    this.gameManager._broadcast();
+  }
+
+  _mutate(mutativeCb: (player: Player) => void) {
+    this._withPointer((pointer) => {
+      mutativeCb(pointer);
+      this._broadcast();
+    });
+  }
+
+  _pointer(): Player | undefined {
+    const operation = this.gameManager._withPointer((pointer) => {
+      const canonicalId = [this.socketId, ...this.aliasSocketIds].find(
+        (id) => pointer.players[id]
+      );
+      return canonicalId ? pointer.players[canonicalId] : undefined;
+    });
+    if (operation.status === "success") {
+      return operation.result;
+    }
+  }
+
+  _set(player: Player): void {
+    this.gameManager.setWithPointer((gamePointer) => ({
+      ...gamePointer,
+      players: {
+        ...gamePointer.players,
+        [this.socketId]: player,
+      },
+    }));
+
+    this._broadcast();
+  }
+
+  _withPointer<T = void>(cb: (playerPointer: Player) => T): Operation<T> {
+    const pointer = this._pointer();
+    if (pointer) {
+      const result = cb(pointer);
+      return { status: "success", result };
+    } else {
+      return { status: "error" };
+    }
+  }
+
+
+  public snapshot(): Player | undefined {
+    const operation = this._withPointer((pointer) => cloneDeep(pointer));
+    if (operation.status === "success") {
+      return operation.result;
+    }
+  }
+
+  public update(mutativeCb: (player: Player) => void) {
+    this._mutate(mutativeCb);
+  }
+}
