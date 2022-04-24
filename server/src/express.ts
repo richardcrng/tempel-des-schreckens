55f21f0e4ab7f7b875308aa232ab98efbdaeee55
Richard Ng <richardcrng@gmail.com>
2022-04-24 18:36:26 +0100

    Redirect GET / from server to client front-end

--- server/src/express.ts
+++ server/src/express.ts
@@ -4,6 +4,10 @@ import cors from "cors";
 const app = express();
 app.use(cors());
 
+app.get("/", (req, res) => {
+  res.redirect("https://tempel.rcr.dev")
+})
+
 app.get("/ping", (req, res) => {
   res.json({ status: "success" });
 });
