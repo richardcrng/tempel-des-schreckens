a9fd719718c0e202b71c38ea56d011c748a3aeea
Richard Ng <richardcrng@gmail.com>
2023-01-05 18:51:40 +0000

    Add border to CardPreview images to be clearer

--- client/src/components/atoms/CardPreview.tsx
+++ client/src/components/atoms/CardPreview.tsx
@@ -1,4 +1,5 @@
 import { Image } from "semantic-ui-react";
+import styled from 'styled-components'
 import { CardType } from "../../types/game.types";
 
 interface Props {
@@ -11,10 +12,19 @@ function CardPreview({ cardType, n }: Props): JSX.Element {
   return (
     <>
       {range.map((n) => (
-        <Image key={n} avatar src={`/assets/tds-${cardType}.jpeg`} />
+        <CardPreviewImage
+          key={n}
+          avatar
+          cardType={cardType}
+          src={`/assets/tds-${cardType}.jpeg`}
+        />
       ))}
     </>
   );
 }
+
+const CardPreviewImage = styled(Image)`
+  border: 3px solid ${({ cardType }: { cardType: CardType }) => cardType === CardType.FIRE ? 'red' : cardType === CardType.GOLD ? 'green' : 'black'};
+`
 
 export default CardPreview;
